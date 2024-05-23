d41c95c04f8c0d32fb4e15f3e34313f0786b7b83
june lee <melodysdreamj@gmail.com>
2024-05-23 19:41:55 +0900

    Update widgets.dart

--- lib/app/data/widgets.dart
+++ lib/app/data/widgets.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
+import '../../widget_book/netflix_main_view_lego/_/_.dart' as netflix_main_view_lego;
 
 final List<Tuple2<String, Widget>> widgets = [
-  Tuple2('First', Container(color: Colors.red)),
-  Tuple2('Second', Container(color: Colors.green)),
-  Tuple2('Third', Container(color: Colors.blue)),
+  Tuple2('netflix_main_view_lego', netflix_main_view_lego.NewView()),
 ];
