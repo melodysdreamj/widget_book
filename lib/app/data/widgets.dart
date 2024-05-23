e3546a9fff4c8dc3c9f5921de9e9c5f5289dcc6b
june lee <melodysdreamj@gmail.com>
2024-05-23 16:52:04 +0900

    `

---
+++ lib/app/data/widgets.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
+
+final List<Tuple2<String, Widget>> widgets = [
+  Tuple2('First', Container(color: Colors.red)),
+  Tuple2('Second', Container(color: Colors.green)),
+  Tuple2('Third', Container(color: Colors.blue)),
+];
