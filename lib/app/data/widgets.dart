972d85479d2ea8d03107ac4876af63fcdf89fae0
june lee <melodysdreamj@gmail.com>
2024-05-24 17:21:51 +0900

    Squashed commit of the following:
    commit 71aac1b4880f2c414f82c9be05e34ed76e82abf3
    Author: june lee <melodysdreamj@gmail.com>
    Date:   Fri May 24 17:21:38 2024 +0900
    
        `
    
    commit a75832f334331a031f62c7c5894fd189aa49abfa
    Author: june lee <melodysdreamj@gmail.com>
    Date:   Fri May 24 10:05:12 2024 +0900
    
        `
    
    commit 60ad85e615c6b6b0b9ae5f691ad96e5f04df3524
    Author: june lee <melodysdreamj@gmail.com>
    Date:   Fri May 24 10:03:02 2024 +0900
    
        `

--- lib/app/data/widgets.dart
+++ lib/app/data/widgets.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
+
+import '../../widget_book/widget_book_view_lego/_/_.dart' as widget_book_view_lego;
+import '../../widget_book/toast_view_lego.toast/usage.dart' as toast_view_lego_toast;
 import '../../widget_book/netflix_main_view_lego/_/_.dart' as netflix_main_view_lego;
 
 final List<Tuple2<String, Widget>> widgets = [
+  Tuple2('widget_book_view_lego', widget_book_view_lego.NewView()),
   Tuple2('netflix_main_view_lego', netflix_main_view_lego.NewView()),
+  Tuple2('toast_view_lego.toast', toast_view_lego_toast.Usage())
 ];
