7eb83771ce1699019ecf18b8eb89fad4fb626fbc
june lee <melodysdreamj@gmail.com>
2024-07-15 03:03:07 +0900

    `

--- lib/app/data/widgets.dart
+++ lib/app/data/widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
+import '../../widget_book/navigation_rail_view_lego/_/_.dart' as navigation_rail_view_lego;
 import '../../widget_book/favorite_button_lego.component/_/_.dart' as favorite_button_lego_component;
 import '../../widget_book/simple_memo_lego/_/_.dart' as simple_memo_lego;
 import '../../widget_book/chrome_style_browser_lego/_/_.dart' as chrome_style_browser_lego;
@@ -9,6 +10,7 @@ import '../../widget_book/toast_view_lego.toast/usage.dart' as toast_view_lego_t
 import '../../widget_book/netflix_main_view_lego/_/_.dart' as netflix_main_view_lego;
 
 final List<Tuple2<String, Widget>> widgets = [
+  Tuple2('navigation_rail_view_lego', navigation_rail_view_lego.NewView()),
   Tuple2('favorite_button_lego.component', favorite_button_lego_component.NewView()),
   Tuple2('simple_memo_lego', simple_memo_lego.NewView()),
   Tuple2('chrome_style_browser_lego', chrome_style_browser_lego.NewView()),
---
+++ lib/widget_book/navigation_rail_view_lego/_/_.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../../../../../../main.dart';
+
+class NewView extends StatefulWidget {
+  const NewView({super.key});
+
+  @override
+  State<NewView> createState() => _NewViewState();
+}
+
+class _NewViewState extends State<NewView> {
+  int _selectedIndex = 0;
+  NavigationRailLabelType labelType = NavigationRailLabelType.all;
+  bool showLeading = false;
+  bool showTrailing = false;
+  double groupAlignment = -1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            groupAlignment: groupAlignment,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: labelType,
+            leading: showLeading
+                ? FloatingActionButton(
+              elevation: 0,
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              child: const Icon(Icons.add),
+            )
+                : const SizedBox(),
+            trailing: showTrailing
+                ? IconButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              icon: const Icon(Icons.more_horiz_rounded),
+            )
+                : const SizedBox(),
+            destinations: const <NavigationRailDestination>[
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite_border),
+                selectedIcon: Icon(Icons.favorite),
+                label: Text('First'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.bookmark_border),
+                selectedIcon: Icon(Icons.book),
+                label: Text('Second'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.star_border),
+                selectedIcon: Icon(Icons.star),
+                label: Text('Third'),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          IndexedStack(
+            index: _selectedIndex,
+            children: [
+              //////////////////////
+              Container().backgroundColor(Colors.red),
+              Container().backgroundColor(Colors.blue),
+              Container().backgroundColor(Colors.yellow),
+              Container().backgroundColor(Colors.green),
+              //////////////////////
+            ],
+          ).expanded(),
+        ],
+      ),
+    );
+  }
+}
+
+main() async {
+  return buildApp(home: NewView());
+}
