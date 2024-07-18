7fe3448f667e51bd2b9b7e6f1f86003ca441e14b
june lee <melodysdreamj@gmail.com>
2024-07-18 17:28:37 +0900

    `

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2427143508"
+    serviceWorkerVersion: "1077984463"
   }
 });
